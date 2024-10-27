2357386072c72418cb276fbdbd86d1f646b1dd18
Amila <amila.residovic@fet.ba>
2024-10-27 12:49:11 +0100

    .

--- Homework1/ZadatakA.kt
+++ Homework1/ZadatakA.kt
@@ -3,15 +3,18 @@ interface Person{
     fun zemlja(): String
 }
 open class Developer(val ime:String,val prezime:String,val godine:Int,val zemlja:String,val progJezik: String): Person{
+    override fun starost(): Int= godine
+    override fun zemlja(): String= zemlja
 
 }
-class BackendDeveloper(): Developer(backendFramework){
+class BackendDeveloper(val ime:String,val prezime:String,val godine:Int,val zemlja:String,val progJezik: String,val backendFramework: String): Developer(ime,prezime,godine,zemlja,progJezik){
 
 }
-class FrontendDeveloper(): Developer(frontendFramework){
+class FrontendDeveloper(val ime:String,val prezime:String,val godine:Int,val zemlja:String,val progJezik: String,val frontendFramework: String): Developer(ime,prezime,godine,zemlja,progJezik){
 
 }
 fun brojJezika(progJezik: List<String>): Map<String,Int>{
+
 
 }
 fun prosjStarost(progJezik: List<String>): Map<String,Double>{
