3893b15696fb746358629b746549ac1d7c4ab3ba
Amila <amila.residovic@fet.ba>
2024-10-29 19:05:12 +0100

    zavrsen zadatak

--- Homework1/ZadatakA.kt
+++ Homework1/ZadatakA.kt
@@ -13,10 +13,43 @@ class BackendDeveloper(val ime:String,val prezime:String,val godine:Int,val zeml
 class FrontendDeveloper(val ime:String,val prezime:String,val godine:Int,val zemlja:String,val progJezik: String,val frontendFramework: String): Developer(ime,prezime,godine,zemlja,progJezik){
 
 }
-fun brojJezika(progJezik: List<String>): Map<String,Int>{
-
-
-}
-fun prosjStarost(progJezik: List<String>): Map<String,Double>{
+fun brojJezik(programeri: List<Developer>): Map<String,Int>{
 
 }
+fun prosjStarost(programeri: List<Developer>): Map<String,Double>{
+
+}
+fun printDeveloperData{
+
+}
+
+fun main() {
+
+    val listaBackendProgramera = listOf(
+        BackendDeveloper("Amila", "Residovic", 23, "BiH", "Kotlin i Spring Boot"),
+        BackendDeveloper("Ibrahim", "Selimovic", 22, "BiH", "Java i Spring"),
+        BackendDeveloper("Edin", "Drapic", 24, "BiH", "Kotlin i Ktor")
+
+    )
+    for (programer in listaBackendProgramera) {
+        println("${programer.ime} je backend programer koji koristi ${programer.progJezik}")
+    }
+    val listaFrontendProgramera = listOf(
+        FrontendDeveloper("Emina", "Jusufovic", 23, "BiH", "Kotlin i React"),
+        FrontendDeveloper("Mujo", "Alic", 22, "BiH", "JavaScript i Vue.js")
+    )
+
+    for (programer in listaFrontendProgramera) {
+        println("${programer.ime} je frontend programer koji koristi ${programer.progJezik}")
+    }
+}
+
+
+
+
+
+
+
+
+
+
